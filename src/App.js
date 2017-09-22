344f91f1059993df8c90cca33bbb2481f5f6a564
jzybert <jzybert@quickbase.com>
2017-09-22 12:40:06 -0400

    boilerplate react app, with knight

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+import Knight from './components/Knight';
+
+export default class App extends Component {
+  render() {
+    return (
+      <Knight/>
+    );
+  }
+}
