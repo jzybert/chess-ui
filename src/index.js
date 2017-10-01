ca5338bb4ed1d3a77c024f86c029422f8b125121
jzybert <jzybert@quickbase.com>
2017-09-30 23:23:41 -0400

    knight movement on click

--- src/index.js
+++ src/index.js
@@ -1,5 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import Board from './components/Board';
+import {observe} from './components/Game'
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const rootEl = document.getElementById('root');
+
+observe(knightPosition =>
+    ReactDOM.render(
+        <Board knightPosition={knightPosition} />,
+        rootEl
+    )
+);
