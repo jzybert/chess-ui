d2813534eb761aec9cc4695b053ebd096b863ec6
jzybert <jzybert@quickbase.com>
2017-10-05 20:58:43 -0400

    drag and drop knight with colors on board

--- src/components/Board.js
+++ src/components/Board.js
@@ -2,35 +2,28 @@ import React, { Component } from 'react';
 import {DragDropContext} from 'react-dnd';
 import HTML5Backend from 'react-dnd-html5-backend';
 import PropTypes from 'prop-types';
-import Square from './Square';
+import BoardSquare from './BoardSquare';
 import Knight from './Knight';
-import {canMoveKnight, moveKnight} from './Game';
 
 class Board extends Component {
     renderSquare(i) {
         const x = i % 8;
         const y = Math.floor(i / 8);
-        const black = (x + y) % 2 === 1;
-
-        const [knightX, knightY] = this.props.knightPosition;
-        const piece = (x === knightX && y === knightY) ?
-            <Knight isBlack={true}/> :
-            null;
 
         return (
             <div key={i}
-                 style={{ width: '12.5%', height: '12.5%' }}
-                 onClick={() => this.handleSquareClick(x, y)}>
-                <Square black={black}>
-                    {piece}
-                </Square>
+                 style={{ width: '12.5%', height: '12.5%' }}>
+                <BoardSquare x={x} y={y}>
+                    {this.renderPiece(x, y)}
+                </BoardSquare>
             </div>
         );
     }
 
-    handleSquareClick(toX, toY) {
-        if (canMoveKnight(toX, toY)) {
-            moveKnight(toX, toY);
+    renderPiece(x, y) {
+        const [knightX, knightY] = this.props.knightPosition;
+        if (x === knightX && y === knightY) {
+            return <Knight isBlack={true}/>;
         }
     }
 
