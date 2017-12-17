98e3eca34094e7583d393131eceb39402e11c54d
jzybert <jmzybert@gmail.com>
2017-12-17 09:38:41 -0800

    movement for all pieces

--- src/components/Game.js
+++ src/components/Game.js
@@ -14,6 +14,10 @@ export function observe(o) {
     emitChange();
 }
 
+export function movePiece(fromX, fromY, toX, toY) {
+
+}
+
 export function moveKnight(toX, toY) {
     knightPosition = [toX, toY];
     emitChange();
@@ -26,4 +30,34 @@ export function canMoveKnight(toX, toY) {
 
     return (Math.abs(dx) === 2 && Math.abs(dy) === 1) ||
            (Math.abs(dx) === 1 && Math.abs(dy) === 2);
+}
+
+export function canMoveBishop(toX, toY) {
+    const [x, y] = bishopPosition;
+    const dx = toX - x;
+    const dy = toY - y;
+
+    return Math.abs(dx) === Math.abs(dy);
+}
+
+export function canMoveRook(toX, toY) {
+    const [x, y] = rookPosition;
+
+    return x === toX || y === toY;
+}
+
+export function canMoveQueen(toX, toY) {
+    const [x, y] = queenPosition;
+    const dx = toX - x;
+    const dy = toY - y;
+
+    return x === toX || y === toY || Math.abs(dx) === Math.abs(dy);
+}
+
+export function canMovePawn(toX, toY) {
+
+}
+
+export function canMoveKing(toX, toY) {
+
 }
