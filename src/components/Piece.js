1d09b1d5e5302e67ee47d9ece14a211450cc0560
jzybert <jmzybert@gmail.com>
2017-12-27 11:45:31 -0800

    piece class

---
+++ src/components/Piece.js
@@ -0,0 +1,45 @@
+import React, {Component} from 'react';
+import {DragSource} from 'react-dnd';
+import PropTypes from 'prop-types';
+import {ItemTypes} from './ItemTypes';
+import {getIcon} from '../util';
+
+const pieceSource = {
+    beginDrag(props) {
+        return {};
+    }
+};
+
+function collect(connect, monitor) {
+    return {
+        connectDragSource: connect.dragSource(),
+        isDragging: monitor.isDragging()
+    }
+}
+
+class Piece extends Component {
+    render() {
+        const {connectDragSource, isDragging, isBlack, pieceType} = this.props;
+        let icon = getIcon(pieceType, isBlack);
+        let piece = <span>{icon}</span>;
+        return connectDragSource(
+            <div style={{
+                opacity: isDragging ? 0.5 : 1,
+                fontSize: 25,
+                fontWeight: 'bold',
+                cursor: 'move'
+            }}>
+                {piece}
+            </div>
+        );
+    }
+}
+
+Piece.propTypes = {
+    connectDragSource: PropTypes.func.isRequired,
+    isDragging: PropTypes.bool.isRequired,
+    isBlack: PropTypes.bool.isRequired,
+    pieceType: PropTypes.string.isRequired
+};
+
+export default DragSource(ItemTypes.PIECE, pieceSource, collect)(Piece);
