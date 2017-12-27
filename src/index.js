eccf84e8c49ace6bb64f611bea83ad342d964a10
jzybert <jmzybert@gmail.com>
2017-12-27 11:31:53 -0800

    utilize react-redux

--- src/index.js
+++ src/index.js
@@ -1,13 +1,27 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {Provider} from 'react-redux';
+import {createStore} from 'redux';
 import Board from './components/Board';
-import {observe} from './components/Game'
+import board from './reducers/board';
+import {ItemTypes} from './components/ItemTypes';
+import {Colors} from './Colors';
 
-const rootEl = document.getElementById('root');
+let store = createStore(board);
 
-observe(knightPosition =>
-    ReactDOM.render(
-        <Board knightPosition={knightPosition} />,
-        rootEl
-    )
+let positions = [
+    {
+        color: Colors.BLACK,
+        piece: ItemTypes.KNIGHT,
+        position: [0, 0]
+    }
+];
+
+ReactDOM.render(
+    <Provider store={store}>
+        <Board
+            piecePositions={positions}
+        />
+    </Provider>,
+    document.getElementById('root')
 );
