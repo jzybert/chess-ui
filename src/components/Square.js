8e24f7683176b23ac773070bd1ec4c9b94202966
jzybert <jmzybert@gmail.com>
2017-12-27 11:37:01 -0800

    squares now shades of brown

--- src/components/Square.js
+++ src/components/Square.js
@@ -4,13 +4,11 @@ import PropTypes from 'prop-types';
 export default class Square extends Component {
     render() {
         const { black } = this.props;
-        const fill = black ? 'black' : 'white';
-        const stroke = black ? 'white' : 'black';
+        const fill = black ? 'rgb(109, 88, 64)' : 'rgb(216, 180, 140)';
 
         return (
             <div style={{
                 backgroundColor: fill,
-                color: stroke,
                 width: '100%',
                 height: '50px'
             }}>
