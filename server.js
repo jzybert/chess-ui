cd6c5e188387231400c45b15a016b295714034f4
jzybert <jmzybert@gmail.com>
2018-04-04 20:49:49 -0400

    Use let instead of var

--- server.js
+++ server.js
@@ -1,10 +1,10 @@
-var path = require('path');
-var webpack = require('webpack');
-var express = require('express');
-var config = require('./webpack.config');
+let path = require('path');
+let webpack = require('webpack');
+let express = require('express');
+let config = require('./webpack.config');
 
-var app = express();
-var compiler = webpack(config);
+let app = express();
+let compiler = webpack(config);
 
 app.use(require('webpack-dev-middleware')(compiler, {
     publicPath: config.output.publicPath
